55dbc43928e3f785f5ca33dae0a7f3164e0267be
mehulcubezytech <mehul.cubezytech@gmail.com>
2022-05-30 12:19:37 +0530

    commit - 5/30/22 change one signal lib.

--- app/src/main/java/com/weatherlive/darkskyweather/utils/ArrowPay.java
+++ app/src/main/java/com/weatherlive/darkskyweather/utils/ArrowPay.java
@@ -1,21 +1,15 @@
 package com.weatherlive.darkskyweather.utils;
 
-import android.annotation.SuppressLint;
+import static com.facebook.ads.BuildConfig.DEBUG;
+
 import android.app.Application;
 import android.content.Context;
 import android.os.StrictMode;
-
 
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.onesignal.OneSignal;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import static com.facebook.ads.BuildConfig.DEBUG;
 
 public class ArrowPay extends Application {
 
